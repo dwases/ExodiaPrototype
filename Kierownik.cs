19bebfd79b8a76000b461ec24b5b8d989198fb3a
CGClloud <162813480+CGClloud@users.noreply.github.com>
2024-06-15 08:37:15 +0200

    Test i zrobiona Inwentaryzacja

---
+++ Kierownik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExodiaPrototype
+{
+
+    public class Kierownik : Pracownik
+    {
+        public Kierownik(string imie, string nazwisko, float placa, bool isInstructor, int id) : base(imie, nazwisko, placa,isInstructor,id)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Placa = placa;
+            IsInstructor = isInstructor;
+            Id = id;
+            grafik = null;
+        }
+        public Schedule AddSchedule(int liczba_dni, Pracownik p, Kierownik k, List<List<TimeOnly>> hours)
+        {
+            Schedule grafik = new Schedule(liczba_dni, p, k);
+            for(int i = 0; i < hours.Count(); i++)
+            {
+                grafik.add_Day(i + 1, hours[i][0], hours[i][1]);
+            }
+            return grafik;
+        }
+        public Szkolenie AddSzkolenie(TimeOnly GodzinaRozpoczecia, TimeOnly GodzinaUkonczenia, string Status, string Details, Pracownik szkolony, Pracownik Instruktor)
+        {
+            Szkolenie s = new Szkolenie(GodzinaRozpoczecia, GodzinaUkonczenia, Status, Details, szkolony, Instruktor);
+            return s;
+        }
+        public void PrzeprowadzInwentaryzacje(List<Polprodukt> stanDniaPoprzedniego, List<Polprodukt> aktualnyStan)
+        {
+            var inwentaryzacja = new Inwentaryzacja
+            {
+                StanDniaPoprzedniego = new Polprodukty { polprodukty = stanDniaPoprzedniego },
+                WartoscNaStanie = new Polprodukty { polprodukty = aktualnyStan }
+            };
+
+            inwentaryzacja.CreateListaStraty();
+            inwentaryzacja.CreateWartoscNaStanie();
+            inwentaryzacja.CreateZuzycie();
+
+            string raport = inwentaryzacja.GenerujRaport();
+            Console.WriteLine(raport);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExodiaPrototype;
+
+//Console.WriteLine("Hello, World!");
+KierownikRestauracji sara = new KierownikRestauracji("Sara", "Kulewicz", 2137.0f, false, 0);
+Kierownik kierownik = new Kierownik("Pan", "Kierownik", 5376.0f, true, 1);
+Pracownik pracownik = new Pracownik("Pani", "Pracowniczka", 4250.0f, false, 2);
+Pracownik instruktor = new Pracownik("Pan", "Instruktor", 4870.0f, true, 3);
+
+ListaPracownikow lista = new ListaPracownikow();
+lista.DodajPracownika(pracownik);
+lista.DodajPracownika(kierownik);
+lista.DodajPracownika(instruktor);
+
+
+
+//testy zwiazane ze skladaniem zamowien:
+    Pracownik pracownikNaKasie = new Pracownik("Kasjer", "Zwyczajny", 4250.0f, false, 5);
+    Pracownik pracownikNaKuchni = new Pracownik("Kucharz", "Pospolity", 4250.0f, false, 6);
+    lista.DodajPracownika(pracownikNaKasie);
+    lista.DodajPracownika(pracownikNaKuchni);
+
+    Order order1 = pracownikNaKasie.AddOrder(pracownikNaKasie, 5, Status.W_Trakcie_Realizacji);
+    ParaProduktLiczba produktLiczba1 = new ParaProduktLiczba(3, new Product("hamburger", 15.20f, "2334"));
+    ParaProduktLiczba produktLiczba2 = new ParaProduktLiczba(6, new Product("woda 1l", 15.20f, "1111"));
+    order1.SkladZamowienia.Add(produktLiczba1);
+    order1.SkladZamowienia.Add(produktLiczba2);
+
+    Order order2 = pracownikNaKasie.AddOrder(pracownikNaKasie, 5, Status.W_Trakcie_Realizacji);
+    ParaProduktLiczba produktLiczba3 = new ParaProduktLiczba(7, new Product("hot dog", 10.00f, "2354"));
+    order2.SkladZamowienia.Add(produktLiczba3);
+
+    Orders orders = new Orders();
+    pracownikNaKasie.PlaceOrder(orders.orders, order1);
+    pracownikNaKasie.PlaceOrder(orders.orders, order2);
+
+    Console.WriteLine("TEST ZAMOWIEN");
+    Console.WriteLine(pracownikNaKuchni.ViewActiveOrders(orders.orders));
+
+    pracownikNaKuchni.FinalizeOrder(order2);
+    Console.WriteLine("TEST ZAMOWIEN po finalizacji jednego z nich:");
+    Console.WriteLine(pracownikNaKuchni.ViewActiveOrders(orders.orders));
+
+
+
+//testy zwiazane ze szkoleniem:
+    TimeOnly start = TimeOnly.FromDateTime(DateTime.Now);
+    TimeOnly end = start.AddHours(3);
+    TimeOnly start2 = TimeOnly.FromDateTime(DateTime.Now);
+    start2 = start2.AddHours(3);
+    TimeOnly end2 = start2.AddHours(3);
+    Szkolenie szkolenie = new Szkolenie(start,end,"0","Szkolenie ze smazenia frytek", pracownik, instruktor);
+    Szkolenie szkolenie2 = new Szkolenie(start2, end2, "0", "Szkolenie z obslugi kasy fiskalnej", pracownikNaKuchni, instruktor);
+    Szkolenia szkolenia = new Szkolenia(kierownik);
+    szkolenia.AddSzkolenie(szkolenie);
+    szkolenia.AddSzkolenie(szkolenie2);
+
+    Console.WriteLine("Test szkolen(kierownik):");
+    Console.WriteLine(szkolenia.ViewSzkolenia(kierownik));
+
+    Console.WriteLine("Test szkolen(szkolony):");
+    Console.WriteLine(pracownik.ZobaczSzkolenie(szkolenie));
+
+
+    Console.WriteLine("Test szkolen(instruktor):");
+    Console.WriteLine(instruktor.ZobaczSzkolenie(szkolenie));
+
+// Przykładowe dane dla inwentaryzacji
+List<Polprodukt> stanDniaPoprzedniego = new List<Polprodukt>
+            {
+                new Polprodukt { Nazwa = "Kotlet", Ilosc = 100 },
+                new Polprodukt { Nazwa = "Frytki", Ilosc = 200 },
+                new Polprodukt { Nazwa = "Napoje", Ilosc = 150 }
+            };
+
+List<Polprodukt> aktualnyStan = new List<Polprodukt>
+            {
+                new Polprodukt { Nazwa = "Kotlet", Ilosc = 80 },
+                new Polprodukt { Nazwa = "Frytki", Ilosc = 170 },
+                new Polprodukt { Nazwa = "Napoje", Ilosc = 140 }
+            };
+
+// Przeprowadzenie testowe inwentaryzacji
+kierownik.PrzeprowadzInwentaryzacje(stanDniaPoprzedniego, aktualnyStan);
+        
